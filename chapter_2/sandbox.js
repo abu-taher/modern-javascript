342a45805c5b5f3a6a4b41bc720bff8b253a6660
abu-taher <iahmed.it@gmail.com>
2020-04-29 01:34:05 +0300

    added comments to explain what are the truthy and falsy values in javascript

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -8,8 +8,8 @@
 // let result = Number('hello');
 // let result = String(50);
 // let result = Boolean(100);
-// let result = Boolean(0);
-// let result = Boolean('0');
-let result = Boolean('');
+// let result = Boolean(0); // any number greater or smaller than zero is a truthy value, zero is a falsy value
+// let result = Boolean('0'); // any string with length is a truthy value
+let result = Boolean(''); // empty string is a falsy value
 
 console.log(result, typeof result);
