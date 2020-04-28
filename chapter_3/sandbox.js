5905697fbef32f88adca6893ed3af1e8f717c73e
abu-taher <iahmed.it@gmail.com>
2020-04-29 02:42:21 +0300

    added an explaination of <continue> and <break> keywords

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -4,14 +4,14 @@ const scores = [50, 25, 0, 30, 100, 20, 10];
 for(let i = 0; i < scores.length; i++){
 
   if(scores[i] === 0){
-    continue;
+    continue; // ignore the rest of the block, back to the top and go to the next iteration
   }
 
   console.log('your score:', scores[i]);
 
   if(scores[i] === 100){
     console.log('congrats, you got the top score!');
-    break;
+    break; // break out of the loop
   }
 
 }
