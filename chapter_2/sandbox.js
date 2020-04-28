3791f96d0ef1750e331b1f333ccfb01e2330e784
abu-taher <iahmed.it@gmail.com>
2020-04-28 21:46:27 +0300

    added another name of a template string

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -8,7 +8,7 @@ const likes = 30;
 // let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
 // console.log(result);
 
-// template string way
+// template string way (Template Literal)
 
 // let result = `The blog called ${title} by ${author} has ${likes} likes`;
 // console.log(result);
