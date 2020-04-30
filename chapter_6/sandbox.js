87f600d9bd78660a325e091f123bda486c7bc670
abu-taher <iahmed.it@gmail.com>
2020-04-30 10:25:52 +0300

    added comments for add, update, and remove styles using javascript

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -5,7 +5,10 @@ const title = document.querySelector('h1');
 console.log(title.style);
 console.log(title.style.color);
 
+// ADD AN EXTRA STYLE
 title.style.margin = '50px';
+// UPDATE AN EXISTING STYLE
 title.style.color = 'crimson';
 title.style.fontSize = '60px';
+// REMOVE AN EXISTING STYLE
 title.style.margin = '';
