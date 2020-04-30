008475912daa20cfef08b5b2adc4ad3f53d8b48c
abu-taher <iahmed.it@gmail.com>
2020-04-30 20:41:46 +0300

    wrote a note of what is the better way to select the item that happened an event on it

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -10,7 +10,7 @@ items.forEach(item => {
   item.addEventListener('click', e => {
     // console.log('item clicked');
     // console.log(e);
-    // console.log(e.target);
+    // console.log(e.target); // better way than others to select the item
     // console.log(item);
     e.target.style.textDecoration = 'line-through';
   });
