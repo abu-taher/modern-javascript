35f787a2e7126433f7dea8bb5afb7ac018083276
abu-taher <iahmed.it@gmail.com>
2020-04-30 09:56:24 +0300

    added a comment of how to add an attribute to an HTML element

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -8,4 +8,6 @@ const mssg = document.querySelector('p');
 
 console.log(mssg.getAttribute('class'));
 mssg.setAttribute('class', 'success');
+
+// IF THE ATTRIBUTE DOESN'T EXIST ALREADY, JAVASCRIPT WILL ADDED IT
 mssg.setAttribute('style', 'color: green');
