b1dcda93518995fe05728bda4f63e3ba619ce8be
abu-taher <iahmed.it@gmail.com>
2020-05-01 17:29:56 +0300

    added some comments to clarify how the submitting a form is working

--- chapter_7/sandbox.js
+++ chapter_7/sandbox.js
@@ -1,5 +1,4 @@
 // WHEN WE ARE LISTENING FOR A SUBMIT EVENT, WE DON'T ATTACH OUR EVENT LISTENER TO THE SUBMIT BUTTON, INSTEAD WE ATTACH IT TO THE FORM
-
 const form = document.querySelector('.signup-form');
 // const username = document.querySelector('#username');
 
@@ -8,7 +7,7 @@ form.addEventListener('submit', e => {
   e.preventDefault();
   // console.log('form submitted');
   // console.log(username.value);
-  
+
   // THIS WAY IS NOT NEEDING TO CATCH THE username INPUT AND SAVE IT TO THE VARIABLE AS THAT ONE ON THE ABOVE, BUT NOTICE THIS WAY IS NEED TO DEFINE name OR id ATTRIBUTE IN THE INPUT HTML FIELD
   console.log(form.username.value);
 });
