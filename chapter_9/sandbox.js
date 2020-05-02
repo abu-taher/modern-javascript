2bd95e9223124c4b680f3cb6f7292f85903255d8
abu-taher <iahmed.it@gmail.com>
2020-05-02 05:28:29 +0300

    added a definition of a find() method

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -1,3 +1,4 @@
+// find() => RETURNS THE VALUE OF THE FIRST ELEMENT INSIDE AN ARRAY THAT PASSES A CERTAIN TEST IN A CALLBACK FUNCTION
 const scores = [10, 5, 0, 40, 60, 10, 20, 70];
 
 const firstHighScore = scores.find(score => score > 50);
