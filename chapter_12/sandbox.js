2562b010e602fed59556846cce67076f424a7c5c
abu-taher <iahmed.it@gmail.com>
2020-05-03 12:04:55 +0300

    added some comments about the fetch api method

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -1,7 +1,9 @@
 // fetch API
 
+// THE WAY THE FETCH API WORKS IS THE PROMISE IS ONLY EVER REJECTED WHEN WE GET SOME KIND OF NETWORK ERROR
 fetch('json/luigi.json').then(response => {
     //console.log(response);
+    // THE json() METHOD GETS THE RESPONSE DATA => THIS RETURN A PROMISE
     return response.json();
   }).then(data => {
     console.log(data);
