bbdea6a95af939f77f3320f5f1e0c48a807dcbcc
abu-taher <iahmed.it@gmail.com>
2020-05-03 09:49:36 +0300

    added some comments about finding the difference between dates and converting the dates into minutes, hours, and days

--- chapter_11/sandbox.js
+++ chapter_11/sandbox.js
@@ -1,12 +1,16 @@
 //const before = new Date('02/01/2019 7:30:59');
+
+// CREATE A TIME IN THE PAST
 const before = new Date('February 1 2019 7:30:59');
 const now = new Date();
 
+// TO FIND OUT THE DIFFERENCE IN TIME BETWEEN TWO DATES
 const diff = now.getTime() - before.getTime();
 // const diff = now - before;
 
 console.log(diff);
 
+// TO CONVERT THE DIFFERENCE IN TIME BETWEEN TWO DATES TO MINUTES, HOURS, AND DAYS
 const mins = Math.round(diff / 1000 / 60);
 const hours = Math.round(mins / 60);
 const days = Math.round(hours / 24);
