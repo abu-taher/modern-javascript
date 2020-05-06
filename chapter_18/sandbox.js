0eef19d7a332826c8d35edb0ae4e110b79acc3fb
abu-taher <iahmed.it@gmail.com>
2020-05-07 02:31:38 +0300

    added some comments to clarify the rest parameter and spread syntax for (arrays and objects)

--- chapter_18/sandbox.js
+++ chapter_18/sandbox.js
@@ -1,4 +1,4 @@
-// rest parameter
+// rest parameter => ALLOWS US TO BUNDLE UP ARGUMENTS INSIDE A FUNCTION INTO A SINGLE ARRAY PARAMETER
 const double = (...nums) => {
   console.log(nums);
   return nums.map(num => num*2);
@@ -7,12 +7,12 @@ const double = (...nums) => {
 const result = double(1,3,5,7,2,4,6,8);
 console.log(result);
 
-// spread syntax (arrays)
+// spread syntax (arrays) => TO SPREAD OUT AN ARRAY INTO IT'S INDEVIDUAL COMPONENTS (OPPOSITE OF REST PARAMETER)
 const people = ['shaun', 'ryu', 'chun-li'];
 const members = ['mario', 'luigi', ...people];
 console.log(members);
 
-// spread syntax (objects)
+// spread syntax (objects) => TO SPREAD OUT AN OBJECT INTO IT'S INDEVIDUAL COMPONENTS (OPPOSITE OF REST PARAMETER)
 const person = { name: 'shaun', age: 30, job: 'net ninja' };
 const personClone = { ...person, location: 'manchester' };
 console.log(person, personClone);
